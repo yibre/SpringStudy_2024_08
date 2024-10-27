ce7275a3048bdddb512531b56d37b8c79c04d61c
yibre <cdoyue@gmail.com>
2024-10-27 19:27:19 +0900

    [FEAT] Appconfig 리팩터링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,10 @@
 package hello.core;
 
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
+import hello.core.member.MemberRepository;
+import hello.core.discount.DiscountPolicy;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
@@ -10,11 +13,22 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
-    public MemberService memberService() {
+    public  MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(
+                new MemoryMemberRepository(),
+                new FixDiscountPolicy());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
