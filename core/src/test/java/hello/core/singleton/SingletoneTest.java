aaec6ab23413c738734b8f43de9f3c23a381a160
yibre <cdoyue@gmail.com>
2025-01-01 16:58:22 +0900

    [FEAT] 싱글톤 컨테이너
    스프링 빈은 99.9% 싱글톤 방식을 사용한다를 이해하면 ok.

--- core/src/test/java/hello/core/singleton/SingletoneTest.java
+++ core/src/test/java/hello/core/singleton/SingletoneTest.java
@@ -4,6 +4,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,6 +30,8 @@ public class SingletoneTest {
     @Test
     @DisplayName("싱글톤 패턴을 적용한 객체 사용")
     void singletoneServiceTest() {
+
+
         //1. 조회: 호출할 때 마다 같은 객체를 반환
         SingletonService singletonService1 = SingletonService.getInstance();
         //2. 조회: 호출할 때 마다 같은 객체를 반환
@@ -38,6 +42,27 @@ public class SingletoneTest {
         // singletonService1 == singletonService2
         assertThat(singletonService1).isSameAs(singletonService2);
         singletonService1.logic();
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 테스트")
+    void springContainer() {
+        // config를 쓰던 기존 방식임
+        // AppConfig appConfig = new AppConfig();
+
+        // 이건 스프링 컨테이너를 쓰는 방식. 스프링은 기본적으로 싱글톤 방식으로 동작한다"를 이해하면 오케이.
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        MemberService memberService1 = ac.getBean("memberService",
+                MemberService.class);
+        //2. 조회: 호출할 때 마다 같은 객체를 반환
+        MemberService memberService2 = ac.getBean("memberService",
+                MemberService.class);
+        //참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+        //memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
 
