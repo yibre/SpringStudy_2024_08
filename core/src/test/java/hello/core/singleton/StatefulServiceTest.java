a674f40fe81050c97db26c3e47b1481d08a7c5e1
yibre <cdoyue@gmail.com>
2025-01-02 19:37:25 +0900

    [FEAT] 싱글톤 방식의 주의점 상
    빈은 싱글턴 컨테이너 방식으로 유지하므로 빈에 특정 값을 유지하도록 만들면 코딩상 문제가 생긴다

---
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,37 @@
+package hello.core.singleton;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+
+public class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingletone() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+        // ThreadA : A 사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+
+        // ThreadB : B 사용자 20000원 주문
+        statefulService2.order("userB", 20000);
+
+        // ThreadA: 사용자 A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = "+price);
+        // 예상과 달리 20000을 주문했다고 나옴
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
